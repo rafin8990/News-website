c65af329587d9cf62650253f8e84c6863335ab9d
rafin8990 <rafin8990@gmail.com>
2022-09-04 01:52:23 +0600

    code resolved and spinner addeded

--- js/news.js
+++ js/news.js
@@ -1,9 +1,17 @@
 /*------------------------------ newsShow area --------------------- */
 const loadNewsPage = async (category_id) => {
-  const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
-  const res = await fetch(url)
-  const data = await res.json()
-  displayNews(data.data)
+  
+  try{
+    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    displayNews(data.data)
+  }
+  catch(e){
+
+    console.log('Fetch error: ', error);
+}
+  
 }
 const displayNews = newsArea => {
   const newsShowerField = document.getElementById('show-news')
@@ -87,8 +95,74 @@ const displayNews = newsArea => {
       
       `
     newsShowerField.appendChild(newsDiv)
-
+    
   })
+  // stop loader 
+  toggleSpiner(false)
+}
+// loadNewsPage(08)
+
+
+/*----------------- catagory area------------------- */
+
+
+const loadCatagory= async()=>{
+  try{
+    const url="https://openapi.programming-hero.com/api/news/categories" 
+    const res=await fetch(url)
+    const data= await res.json()
+    displayCatagory(data.data.news_category)
+  }
+  catch(e){
+    console.log('Fetch error: ', error);
+  }
   
 }
+
+const displayCatagory=catagories=>{
+const catagoryField=document.getElementById('catagory-field')
+catagoryField.innerHTML=''
+catagories.forEach(catagory=>{
+  // console.log(catagory)
+  const catagoryDiv=document.createElement('div')
+  
+  catagoryDiv.innerHTML=`
+  <div class="btn ps-3 pe-3" onclick="loadNewsPage('${catagory.category_id}')"> ${catagory.category_name}</div>
+  
+
+
+  `
+  
+  catagoryField.appendChild(catagoryDiv)
+  
+})
+// loader start 
+toggleSpiner(true)
+}
+
+
+
+
+loadCatagory()
+
+const toggleSpiner=isloading=>{
+  const loaderSection=document.getElementById('loader')
+  if(isloading){
+      loaderSection.classlist.remove('d-none')
+  }
+  else{
+      loaderSection.classlist.add('d-none')
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
 
